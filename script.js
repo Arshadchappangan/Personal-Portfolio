0fb8947d7db62f746a929d85c05b6e507a751191
Arshad Chappangan <arshadchappangan349@gmail.com>
2024-12-01 16:43:02 +0530

    before mail

---
+++ script.js
@@ -0,0 +1,58 @@
+function openMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+}
+
+$(document).ready(function(){
+    $("#gform").validate({
+        rules:{
+            name:{
+                required:true,
+                minlength:3
+            },
+            place:{
+                required:true,
+                minlength:3
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            phone:{
+                required:true,
+                minlength:10,
+                maxlength:10,
+                digits:true
+            },
+            message:{
+                required:true,
+                minlength:20,
+                maxlength:100
+            }
+        },
+        messages:{
+            phone:{
+                minlength:"Enter a valid phone number",
+                maxlength:"Enter a valid phone number"
+            }
+        }
+    })
+})
+
+
+$("#gform").submit((e) => {
+    e.preventDefault()
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbxPgOdKDtpDUYOnf9agmvklIw2StUVo_pZsKHV9mJMAHyBCwsQaIYQbifl4Z5cltjDJ/exec",
+        data: $("#gform").serialize(),
+        method: "post",
+        success: function (response) {
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error: function (err) {
+            alert("Something Error")
+        }
+    })
+})
